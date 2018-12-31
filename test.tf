00711168dc60a5fceedd2400dc816d3379ab45fa
Matthew Amos <mamos@scottlogic.com>
2018-12-31 13:51:45 +0000

    adding tf to project

---
+++ test.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "s3" {
+    bucket = "sl-terraform-backend"
+    key = "terraform.tfstate"
+    region = "eu-west-1"
+  }
+}
+
+variable "aws_region" {
+  default = "eu-west-1"
+}
+
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+resource "aws_s3_bucket" "test" {
+  bucket = "${terraform.workspace}-test-bucket"
+  acl = "private"
+}
